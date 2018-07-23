40e74075d3ab70dc8d5a589996791111cdc0cb1c
Ryan Walder <ryan.walder@gmail.com>
2018-07-23 12:23:50 +0100

    Update tests

--- test/integration/default/serverspec/i3wm_spec.rb
+++ test/integration/default/serverspec/i3wm_spec.rb
@@ -2,13 +2,16 @@ require 'serverspec'
 
 set :backend, :exec
 
-if os[:family] == 'redhat'
-  i3 = 'i3'
-else
-  i3 = 'i3-wm'
+describe file('/etc/apt/sources.list.d/i3.list') do
+  it { should be_file }
+  its(:content) { should match 'deb http://debian.sur5r.net/i3' }
 end
 
-describe package(i3) do
+describe package('i3-wm') do
+  it { should be_installed }
+end
+
+describe package('xorg') do
   it { should be_installed }
 end
 
